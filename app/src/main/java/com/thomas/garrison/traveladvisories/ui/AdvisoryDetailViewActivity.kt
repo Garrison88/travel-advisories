9ba5c4c55f89bb1acf8d4cd18fe24eedcebd805d
Garrison88 <garrison.siberry@gmail.com>
2018-09-27 09:36:49 -0400

    work on date validation

--- app/src/main/java/com/thomas/garrison/traveladvisories/ui/AdvisoryDetailViewActivity.kt
+++ app/src/main/java/com/thomas/garrison/traveladvisories/ui/AdvisoryDetailViewActivity.kt
@@ -2,6 +2,8 @@ package com.thomas.garrison.traveladvisories.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 import com.thomas.garrison.traveladvisories.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,11 +21,18 @@ class AdvisoryDetailViewActivity : AppCompatActivity() {
         val country = intent.getStringExtra("country") ?: ""
         toolbar.title = country
 
+        val flagImage = findViewById<ImageView>(R.id.flag_image)
+
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24px)
 
         toolbar.setNavigationOnClickListener {
             finish()
         }
+        Picasso.with(applicationContext)
+                .load("https://www.countryflags.io/be/flat/64.png")
+                .resize(500, 500)
+                .centerCrop()
+                .into(flagImage)
 
 //        fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- app/src/main/java/com/thomas/garrison/traveladvisories/ui/MainActivity.kt
+++ app/src/main/java/com/thomas/garrison/traveladvisories/ui/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.thomas.garrison.traveladvisories.ui
 
+import android.app.DatePickerDialog
 import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 import com.thomas.garrison.traveladvisories.R
 import com.thomas.garrison.traveladvisories.database.AppDatabase
+import com.thomas.garrison.traveladvisories.database.Trip
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), AdvisoriesFragment.OnFragmentInteractionListener, TripsFragment.OnFragmentInteractionListener {
@@ -28,9 +36,6 @@ class MainActivity : AppCompatActivity(), AdvisoriesFragment.OnFragmentInteracti
      * [android.support.v4.app.FragmentStatePagerAdapter].
      */
     private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
-
-    private val fm: FragmentManager = supportFragmentManager
-    private val addTripFragment = AddTripDialogFragment()
 
     companion object {
         var database: AppDatabase? = null
@@ -49,7 +54,7 @@ class MainActivity : AppCompatActivity(), AdvisoriesFragment.OnFragmentInteracti
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
 
-        fab.setOnClickListener { addTripFragment.show(fm, "AddTripFragment_tag") }
+        fab.setOnClickListener { openAddTripDialog() }
 
     }
 
@@ -94,4 +99,82 @@ class MainActivity : AppCompatActivity(), AdvisoriesFragment.OnFragmentInteracti
             return 2
         }
     }
+
+    private fun openAddTripDialog () {
+
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        val dialogView = layoutInflater.inflate(R.layout.fragment_add_trip, null)
+
+        val countriesArray = resources.getStringArray(R.array.countries_array)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item, countriesArray)
+        val autoCompleteTextView = dialogView.findViewById<AutoCompleteTextView>(R.id.auto_tv_countries)
+        autoCompleteTextView.threshold = 1
+        autoCompleteTextView.setAdapter(adapter)
+
+        val btnChooseStartDate = dialogView.findViewById<Button>(R.id.btn_start_date)
+        val btnChooseEndDate = dialogView.findViewById<Button>(R.id.btn_end_date)
+
+        var startTime = 0
+        var endTime = 0
+
+        btnChooseStartDate.setOnClickListener {
+            startTime = pickDate(btnChooseStartDate)
+        }
+        btnChooseEndDate.setOnClickListener {
+            endTime = pickDate(btnChooseEndDate)
+        }
+
+        dialogBuilder.setView(dialogView)
+
+                .setPositiveButton("Save") { dialog, id ->
+//                    Log.d("$#@!", startTime.toString())
+//                    Log.d("$#@!", endTime.toString())
+//                    if (startTime < endTime) {
+                        addTrip(autoCompleteTextView.text.toString(),
+                                "Starts on" + btnChooseStartDate.text.toString(),
+                                "Ends on" + btnChooseEndDate.text.toString())
+                        dialog.dismiss()
+//                    } else {
+//                        Toast.makeText(applicationContext, "Try again", Toast.LENGTH_SHORT).show()
+//                    }
+                }
+
+                .setNegativeButton("Cancel") { dialog, id ->
+                    dialog.cancel()
+                }
+                .setCancelable(false)
+
+        val alert = dialogBuilder.create()
+        alert.setTitle("Add Trip")
+        alert.show()
+    }
+
+    private fun addTrip(country: String, startDate: String, endDate: String) {
+        val trip = Trip(0, country, startDate, endDate)
+
+        MainActivity.database?.tripDao()?.insert(trip)
+    }
+
+    private fun pickDate(btn : Button): Int {
+        val cal = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            val sdf = SimpleDateFormat(" MMM dd, yyyy", Locale.ENGLISH)
+//            Log.d("!@#$ TIME", ().toString())
+            btn.text = sdf.format(cal.time)
+        }
+        DatePickerDialog(this,
+            dateSetListener,
+            // default to today's date
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)).show()
+
+        return cal.get(Calendar.YEAR) + cal.get(Calendar.DATE) + cal.get(Calendar.DAY_OF_MONTH)
+
+    }
+
 }
